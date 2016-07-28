e9aebbf0016c98c04a94dcacfacbbfc13f3e578a
Thelordofdream <574483817@qq.com>
2016-07-29 00:08:07 +0800

    Renormalization

--- NLP_doc2vec.py
+++ NLP_doc2vec.py
@@ -15,48 +15,7 @@ import matplotlib.pyplot as plt
 data_path = "review_polarity/txt_sentoken/"
 pos_files = os.listdir(data_path + "pos/")
 neg_files = os.listdir(data_path + "neg/")
-pos_reviews = []
-neg_reviews = []
-unsup_reviews1 = []
-unsup_reviews2 = []
-unsup_reviews = []
 
-for pos_file in pos_files[:900]:
-    with open(data_path + "pos/" + pos_file, 'r') as infile:
-        each = ''
-        each_article = infile.readlines()
-        for each_sentence in each_article:
-            each += each_sentence
-    pos_reviews.extend([each])
-for neg_file in neg_files[:900]:
-    with open(data_path + "neg/" + neg_file, 'r') as infile:
-        each = ''
-        each_article = infile.readlines()
-        for each_sentence in each_article:
-            each += each_sentence
-    neg_reviews.extend([each])
-for pos_file in pos_files[900:]:
-    with open(data_path + "pos/" + pos_file, 'r') as infile:
-        each = ''
-        each_article = infile.readlines()
-        for each_sentence in each_article:
-            each += each_sentence
-    unsup_reviews1.extend([each])
-for neg_file in neg_files[900:]:
-    with open(data_path + "neg/" + neg_file, 'r') as infile:
-        each = ''
-        each_article = infile.readlines()
-        for each_sentence in each_article:
-            each += each_sentence
-    unsup_reviews2.extend([each])
-
-
-# use 1 for positive sentiment, 0 for negative
-y = np.concatenate((np.ones(len(pos_reviews)), np.zeros(len(neg_reviews))))
-
-x_train, x_test, y_train, y_test = train_test_split(np.concatenate((pos_reviews, neg_reviews)), y, test_size=0.2)
-unsup_reviews.extend(unsup_reviews1)
-unsup_reviews.extend(unsup_reviews2)
 
 
 # Do some very minor text preprocessing
@@ -72,11 +31,6 @@ def cleanText(corpus):
     return corpus
 
 
-x_train = cleanText(x_train)
-x_test = cleanText(x_test)
-unsup_reviews = cleanText(unsup_reviews)
-
-
 # Gensim's Doc2Vec implementation requires each document/paragraph to have a label associated with it.
 # We do this by using the LabeledSentence method. The format will be "TRAIN_i" or "TEST_i" where "i" is
 # a dummy index of the review.
@@ -88,10 +42,88 @@ def labelizeReviews(reviews, label_type):
     return labelized
 
 
+# Get training set vectors from our models
+def getVecs(model, corpus, size):
+    vecs = []
+    for z in corpus:
+        vec = np.zeros(size).reshape((1, size))
+        count = 0.
+        for each in np.array(model[z.words]):
+            try:
+                vec += each.reshape((1, size))
+                count += 1.
+            except KeyError:
+                continue
+        if count != 0:
+            vec /= count
+        vecs.extend(vec)
+    return np.array(vecs)
+
+
+def storeVecs(input, filename):
+    import pickle
+    fw = open(filename, 'w')
+    pickle.dump(input, fw)
+    fw.close()
+
+
+def load_sup_set():
+    pos_reviews = []
+    neg_reviews = []
+    for pos_file in pos_files[:900]:
+        with open(data_path + "pos/" + pos_file, 'r') as infile:
+            each = ''
+            each_article = infile.readlines()
+            for each_sentence in each_article:
+                each += each_sentence
+        pos_reviews.extend([each])
+    for neg_file in neg_files[:900]:
+        with open(data_path + "neg/" + neg_file, 'r') as infile:
+            each = ''
+            each_article = infile.readlines()
+            for each_sentence in each_article:
+                each += each_sentence
+        neg_reviews.extend([each])
+    # use 1 for positive sentiment, 0 for negative
+    y = np.concatenate((np.ones(len(pos_reviews)), np.zeros(len(neg_reviews))))
+    x_train, x_test, y_train, y_test = train_test_split(np.concatenate((pos_reviews, neg_reviews)), y, test_size=0.2)
+    return x_train, x_test, y_train, y_test
+
+
+def load_unsup_set():
+    unsup_reviews1 = []
+    unsup_reviews2 = []
+    unsup_reviews = []
+    for pos_file in pos_files[900:]:
+        with open(data_path + "pos/" + pos_file, 'r') as infile:
+            each = ''
+            each_article = infile.readlines()
+            for each_sentence in each_article:
+                each += each_sentence
+        unsup_reviews1.extend([each])
+    for neg_file in neg_files[900:]:
+        with open(data_path + "neg/" + neg_file, 'r') as infile:
+            each = ''
+            each_article = infile.readlines()
+            for each_sentence in each_article:
+                each += each_sentence
+        unsup_reviews2.extend([each])
+    unsup_reviews.extend(unsup_reviews1)
+    unsup_reviews.extend(unsup_reviews2)
+    return unsup_reviews
+
+# load data
+x_train, x_test, y_train, y_test = load_sup_set()
+unsup_reviews = load_unsup_set()
+# Pretreatment
+x_train = cleanText(x_train)
+x_test = cleanText(x_test)
+unsup_reviews = cleanText(unsup_reviews)
 x_train = labelizeReviews(x_train, 'TRAIN')
 x_test = labelizeReviews(x_test, 'TEST')
 unsup_reviews = labelizeReviews(unsup_reviews, 'UNSUP')
 
+# Train Doc2Vec models
 size = 400
 iteration = 5
 
@@ -119,37 +151,19 @@ for epoch in range(iteration):
     model_dbow.train(temp2)
 
 
-# Get training set vectors from our models
-def getVecs(model, corpus, size):
-    vecs = []
-    for z in corpus:
-        vec = np.zeros(size).reshape((1, size))
-        count = 0.
-        for each in np.array(model[z.words]):
-            try:
-                vec += each.reshape((1, size))
-                count += 1.
-            except KeyError:
-                continue
-        if count != 0:
-            vec /= count
-        vecs.extend(vec)
-    return np.array(vecs)
-
-
 train_vecs_dm = getVecs(model_dm, x_train, size)
 train_vecs_dbow = getVecs(model_dbow, x_train, size)
 train_vecs = np.hstack((train_vecs_dm, train_vecs_dbow))
 
 # train over test set
-# x_test = np.array(x_test)
-# temp3 = x_test[:]
-# print "+===== Testing =====+"
-# for epoch in range(iteration):
-#    print "+=== Iteration %d ===+" % epoch
-#    random.shuffle(temp3)
-#    model_dm.train(temp3)
-#    model_dbow.train(temp3)
+x_test = np.array(x_test)
+temp3 = x_test[:]
+print "+===== Testing =====+"
+for epoch in range(iteration):
+    print "+=== Iteration %d ===+" % epoch
+    random.shuffle(temp3)
+    model_dm.train(temp3)
+    model_dbow.train(temp3)
 
 # Construct vectors for test reviews
 test_vecs_dm = getVecs(model_dm, x_test, size)
@@ -157,17 +171,12 @@ test_vecs_dbow = getVecs(model_dbow, x_test, size)
 test_vecs = np.hstack((test_vecs_dm, test_vecs_dbow))
 
 
-def storeVecs(input, filename):
-    import pickle
-    fw = open(filename, 'w')
-    pickle.dump(input, fw)
-    fw.close()
-
 storeVecs(test_vecs, 'test_vecs.txt')
 storeVecs(train_vecs, 'train_vecs.txt')
 storeVecs(y_test, 'y_test.txt')
 storeVecs(y_train, 'y_train.txt')
 
+# Logistic Regression
 lr = SGDClassifier(loss='log', penalty='l1')
 lr.fit(train_vecs, y_train)
 print 'Test Accuracy of Logistic: %.2f' % lr.score(test_vecs, y_test)
@@ -185,6 +194,7 @@ plt.ylim([0.0, 1.05])
 plt.legend(loc='lower right')
 plt.show()
 
+# SVM
 svm = SVC(C=50, gamma=1, probability=True)
 svm.fit(train_vecs, y_train)
 print 'Test Accuracy of SVM: %.2f' % svm.score(test_vecs, y_test)
@@ -203,6 +213,7 @@ plt.ylim([0.0, 1.05])
 plt.legend(loc='lower right')
 plt.show()
 
+# KNN
 knn = neighbors.KNeighborsClassifier(15, weights='distance')
 knn.fit(train_vecs, y_train)
 print 'Test Accuracy of knn: %.2f' % knn.score(test_vecs, y_test)
